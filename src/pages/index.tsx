90bd39a0c979fb1b86d412c4fd3280ea833bcaeb
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-03-10 21:44:44 +0700

    fix: fix error build by replacing ' char with &lsquo;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,7 +87,7 @@ export default function Home() {
         <title>Nametionalize - Predict your nationality by your name</title>
         <meta
           name="description"
-          content="Have you ever wondered if your name could reveal something about nationality? Yes, let's play a little game of prediction! Based on your name, what do you think your nationality might be?"
+          content="Have you ever wondered if your name could reveal something about nationality? Yes, let&lsquo;s play a little game of prediction! Based on your name, what do you think your nationality might be?"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
