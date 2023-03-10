74d6d393e73059cd2c0145a1d55cc43477e6ff8e
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-03-10 10:50:36 +0700

    feat: display the country name by regionName function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 interface ResultType {
   country_id: string;
@@ -12,34 +12,58 @@ export default function Home() {
   const [result, setResult] = useState<Array<ResultType>>([]);
   const [countryId, setCountryId] = useState<string>("");
   const [flagUrl, setFlagUrl] = useState<string[]>([]);
+  const [goSearch, setGoSearch] = useState<boolean>(false);
+  const [loadingFlag, setLoadingFlag] = useState<boolean>(true);
 
   useEffect(() => {
+    setGoSearch(false);
     fetch(`https://api.nationalize.io?name=${name}`)
       .then((res) => res.json())
-      .then(
-        (data) => (
-          setResult(data.country), setCountryId(data?.country[0]?.country_id)
-        )
-      );
-  }, [name]);
+      .then((data) => {
+        const fixedResult = data.country?.map((country: any) => {
+          if (country.country_id === "SQ") {
+            return { ...country, country_id: "SG" };
+          } else {
+            return country;
+          }
+        });
+        setResult(fixedResult);
+        if (fixedResult?.length > 0) {
+          setCountryId(fixedResult[0]?.country_id);
+        }
+      });
+  }, [goSearch]);
+
+  const regionName = (countryId: string) => {
+    const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+    return regionNames.of(countryId);
+  };
+
+  const flagsUrl = useMemo(() => {
+    return result?.map(
+      (item) => `https://countryflagsapi.com/png/${item.country_id}`
+    );
+  }, [result]);
 
   useEffect(() => {
-    if (result.length > 0) {
-      const countryIds = result.map((item) => item.country_id);
+    if (result?.length > 0) {
+      setLoadingFlag(true);
       Promise.all(
-        countryIds.map((countryId) =>
-          fetch(`https://countryflagsapi.com/png/${countryId}`, {
+        flagsUrl.map((flagUrl) =>
+          fetch(flagUrl, {
             method: "GET",
             headers: {
-              "Content-Type": "application/json",
+              "Content-Type": "image/png",
             },
           }).then((res) => res.blob())
         )
-      ).then((blobs) =>
-        setFlagUrl(blobs.map((blob) => URL.createObjectURL(blob)))
-      );
+      )
+        .then((blobs) =>
+          setFlagUrl(blobs.map((blob) => URL.createObjectURL(blob)))
+        )
+        .finally(() => setLoadingFlag(false));
     }
-  }, [result]);
+  }, [result, flagsUrl]);
 
   return (
     <>
@@ -52,7 +76,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="wrapper">
+      <main className="wrapper min-h-screen bg-slate-200">
         <div className="py-4">
           <h1 className="text-2xl text-stone-800 mb-1">Nametionalize</h1>
           <p className="text-sm text-stone-800">
@@ -63,26 +87,42 @@ export default function Home() {
           <div className="h-10 flex items-center my-4">
             <input
               type="text"
+              placeholder="NAME"
               value={name.toUpperCase()}
               onChange={(e) => setName(e.target.value)}
               className="w-full h-full text-sm font-bold text-stone-800 border border-stone-400 outline-none rounded-sm py-1 px-2"
             />
-            <button className="h-full font-bold text-white bg-stone-800 px-6">
+            <button
+              className="h-full font-bold text-white bg-stone-800 px-6"
+              onClick={() => setGoSearch(true)}
+            >
               Go!
             </button>
           </div>
           <div className="flex flex-col gap-2">
-            {result.map((item, index) => (
-              <div key={item.country_id}>
-                <p className="text-sm text-stone-800">
-                  Country: {item.country_id}
-                </p>
-                <img src={flagUrl[index]} alt="flag" className="w-12" />
-                <p className="text-sm text-stone-800">
-                  Probability: {item.probability}
-                </p>
-              </div>
-            ))}
+            {result &&
+              result.map((item, index) => (
+                <div key={item.country_id}>
+                  <p className="text-sm text-stone-800">
+                    {regionName(item.country_id)}
+                  </p>
+                  {loadingFlag ? (
+                    <div className="w-10 h-5 bg-stone-200"></div>
+                  ) : (
+                    <div className="w-10 h-6 relative">
+                      <Image
+                        src={flagUrl[index]}
+                        fill
+                        object-fit="cover"
+                        alt="flag"
+                      />
+                    </div>
+                  )}
+                  <p className="text-sm text-stone-800">
+                    Probability: {item.probability}
+                  </p>
+                </div>
+              ))}
           </div>
         </div>
       </main>
